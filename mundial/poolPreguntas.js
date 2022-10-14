446abe783ecf20335168132348b34cc451984082
MxOxP <hardy_acion@hotmail.com>
2022-10-14 03:59:50 -0500

    NuevoCommit2

--- mundial/poolPreguntas.js
+++ mundial/poolPreguntas.js
@@ -79,6 +79,159 @@ let pool = [
             "Alemania",
             "Brasil"
         ],
+    },
+    {
+        "preg": "¿Qué jugador ganó el Guante de Oro en el Mundial de Rusia 2018?",
+        "res": [
+            "Thibaut Courtois",
+            "Keylor Navas",
+            "Guillermo Ochoa",
+            "Manuel Neuer"
+        ],
+    },
+    {
+        "preg": "¿Quién escribió la famosa novela de Drácula?",
+        "res": [
+            "Bram Stoker",
+            "Juan Rulfo",
+            "Diego Rivera",
+            "Christian Caballero"
+        ],
+    },
+    {
+        "preg": "¿Cuándo se jugará la final de la Copa del Mundo Catar 2022?",
+        "res": [
+            "18 de Diciembre 2022",
+            "24 de Diciembre 2022",
+            "13 de Diciembre 2022",
+            "10 de Diciembre 2022"
+        ],
+    },
+    {
+        "preg": "¿Cuántos años tiene un lustro?",
+        "res": [
+            "Cinco",
+            "Seis",
+            "Cien",
+            "Dos"
+        ],
+    },
+    {
+        "preg": "¿Cuántas Copas del Mundo ha ganado Holanda?",
+        "res": [
+            "0",
+            "1",
+            "2",
+            "3"
+        ],
+    },
+    {
+        "preg": "¿A quién le crecia la nariz cuando mentía?",
+        "res": [
+            "A pinocho",
+            "A la bella durmiente",
+            "Al rey león",
+            "A pumba"
+        ],
+    },
+    {
+        "preg": "¿Cuál es la moneda oficial de Estados Unidos?",
+        "res": [
+            "Dólar",
+            "Yen",
+            "Libra",
+            "Euro"
+        ],
+    },
+    {
+        "preg": "¿En qué año inició la Primera Guerra Mundial?",
+        "res": [
+            "1914",
+            "1924",
+            "1921",
+            "1912"
+        ],
+    },
+    {
+        "preg": "¿Cuál es el órgano más grande del cuerpo humano?",
+        "res": [
+            "La piel",
+            "Corazón",
+            "Pierna",
+            "Hígado"
+        ],
+    },
+    {
+        "preg": "¿Contra qué equipo anontó Maradona el gol 'la mano de Dios'?",
+        "res": [
+            "Inglaterra",
+            "Brasil",
+            "Alemania",
+            "Italia"
+        ],
+    },
+    {
+        "preg": "Nombre de a Mascota oficial para el Mundial de México 1970",
+        "res": [
+            "Juanito",
+            "Naranjito",
+            "Chile",
+            "Sombrerín"
+        ],
+    },
+    {
+        "preg": "Edificio más alto del mundo.",
+        "res": [
+            "El Burj Khalifa",
+            "Torre de Shanghái",
+            "Goldin Finance 117",
+            "Lotte World Tower"
+        ],
+    },
+    {
+        "preg": "¿Quién ha ganado el Mundial como jugador y como entrenador?",
+        "res": [
+            "Mário Zagallo",
+            "Zinedine Zidane",
+            "Marco van Basten",
+            "Romário"
+        ],
+    },
+    {
+        "preg": "¿En dónde se organizó la segundO Copa del Mundo de la Historia",
+        "res": [
+            "Italia 1934",
+            "Argentina 1934",
+            "Perú 1934",
+            "Francia 1934"
+        ],
+    },
+    {
+        "preg": "¿Cuáles son los únicos mamíferos que pueden volar?",
+        "res": [
+            "Murciélagos",
+            "Búhos",
+            "Palomas",
+            "Águilas"
+        ],
+    },
+    {
+        "preg": "¿Cuál es el idioma más antiguo en Europa?",
+        "res": [
+            "El vasco",
+            "Francés",
+            "Castellano",
+            "Italiano"
+        ],
+    },
+    {
+        "preg": "¿Qué dos países hicieron su debut en la Copa del Mundo de 2018?",
+        "res": [
+            "Islandia y Panamá",
+            "Serbia y Croacia",
+            "Costa Rica y Marruecos",
+            "Camerún y Holanda"
+        ],
     }
 ];
 
@@ -91,6 +244,23 @@ let poolBasico = [
             "Verde",
             "Café"
         ]
+    },
+    {
+        "preg": "¿Qué categorías tiene Pétalo®?",
+        "res": [
+            "Papel higiénico, servilletas, servitoallas® y toallas para manos",
+            "Detergente",
+            "Papas y Botanas",
+            "Limpiadores de vidrio y cocina"
+        ],
+    },
+    {
+        "preg": "¿Cómo se llama el perro de Pétalo®?",
+        "res": [
+            "Poppy",
+            "Barney",
+            "Firulais",
+            "Spike"
+        ],
     }
-
 ];
